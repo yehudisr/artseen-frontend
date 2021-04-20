3c4f0652f51a9c6c436f10cd336be9a484d78c4b
LindaHendricks <linda.timoh@gmail.com>
2021-04-20 14:14:01 -0400

    small changing

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -4,14 +4,34 @@ import ReactDOM from 'react-dom';
 function LoginForm({ setLoggedIn }){
         const [username, setUsername] = useState("");
         const [password, setPassword] = useState("");
-         function handleSubmit(event) {
+         
+
+       
+        
+        function handleSubmit(event) {
            event.preventDefault();
              const formData = { 
              username,
              password,
            }
+
            console.log(formData);
+
          }
+
+         fetch(/* blabla, */ { 
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+            'Accept': 'application/json'
+    
+          },
+          Body: JSON.stringify(formData)
+          .then(response => response.json)
+          .then(response => console.log(response))
+        }) 
+
+
          return (
            <form onSubmit={handleSubmit}>
              <h1>Login</h1>
